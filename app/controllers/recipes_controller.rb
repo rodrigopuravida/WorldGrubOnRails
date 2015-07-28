9665cac9551790d911900fc9e2ef4b38264a7416
rodrigopuravida <rodrigo.puravida@gmail.com>
2015-07-28 12:37:33 -0700

    Json ingredients working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,18 +6,43 @@ class RecipesController < ApplicationController
     
     
     def index
-       response = Unirest.get "https://webknox-recipes.p.mashape.com/recipes/search?cuisine=italian&number=100&offset=0&query=pasta%2C+beef",        
+       response = Unirest.get "https://webknox-recipes.p.mashape.com/recipes/search?cuisine=italian&number=25&offset=0&query=pasta%2C+beef",        
   headers:{
     "X-Mashape-Key" => ENV["RECIPE_API"],
     "Accept" => "application/json"
-  }
-   
+  }  
 ##        @data = response.body
         @data = response.body["results"];
         @baseUri= response.body["baseUri"];
-        ##        @data = JSON.parse(response.body) // no good
-#        @baseUri = response.body["baseuri"];
-#        
+              
+#        begin of call to api where we will get ingredients info
+        recipesIds = [];
+        allRecipesInfo = [];
+#        adds all recipe ids to the array
+        @data.each { |x| recipesIds.push(x["id"])};
+#        Now iterate over array to get ingredients
+        
+        recipesIds.each { |recipeId| 
+            response2 = Unirest.get "https://webknox-recipes.p.mashape.com/recipes/" + recipeId.to_s + "/information",
+            headers:{
+            "X-Mashape-Key" => ENV["RECIPE_API"],
+            "Accept" => "application/json"
+            }
+            @dataRecipeInfo = response2.body["extendedIngredients"];
+            allRecipesInfo.push(@dataRecipeInfo);
+        };
+        render json: @dataRecipeInfo;
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
 #        render json: @data;
-    end
+#    end of inddex function
+    end 
 end
